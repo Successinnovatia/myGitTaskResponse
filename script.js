c2ed3ec811d1e8b5749c2312b3dedcc55203a0c4
Divine Success <divinesuccess228@gmail.com>
2021-04-11 17:27:07 +0100

    responsive webpage submission- first deployment

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navlinks = document.getElementsByClassName('nav-links')[0]
+toggleButton.addEventListener('click', () =>{
+    navlinks.classList.toggle('active')
+})
